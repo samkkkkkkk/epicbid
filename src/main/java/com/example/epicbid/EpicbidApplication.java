09bd9b67feaac5ddbaaab27c60906b64f273a25c
SamK5678 <rnflrntk@naver.com>
2026-03-16 18:53:26 +0900

    epicbid 프로젝트 초기 설정

---
+++ src/main/java/com/example/epicbid/EpicbidApplication.java
@@ -0,0 +1,13 @@
+package com.example.epicbid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicbidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicbidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/epicbid/EpicbidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.epicbid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicbidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
